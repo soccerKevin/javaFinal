e1186b89d5e91718ecb965eaa7a873521f7d2492
Brad Farr <v-bfarr@expedia.com>
2017-06-08 15:59:21 -0700

    testing codepipeline

--- src/main/java/com/codefellows/Application.java
+++ src/main/java/com/codefellows/Application.java
@@ -1,13 +1,6 @@
 package com.codefellows;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import static spark.Spark.*;
 
 public class Application {
 
@@ -15,21 +8,9 @@ public class Application {
         return Integer.parseInt(System.getenv().get("PORT"));
     }
 
-    public static void main(String[] args) throws Exception {
-        Server server = new Server(5000); //
-        ServletHandler handler = new ServletHandler();
-        handler.addServletWithMapping(HomeServlet.class, "/*");
-        server.setHandler(handler);
-        server.start();
-        server.join();
-    }
-
-    public static class HomeServlet extends HttpServlet {
-        @Override
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            response.setContentType("text/html;charset=utf-8");
-            response.setStatus(HttpServletResponse.SC_OK);
-            response.getWriter().println("<p> Hello world! AWS demo is running!</p>");
-        }
+    public static void main(String[] args) {
+        get("/hello/:name", (request, response) -> {
+            return "Hello: " + request.params(":name");
+        });
     }
 }
