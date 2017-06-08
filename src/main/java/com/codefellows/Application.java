88291d7a3fdf4e324b3d18a3a19e7ddebdd3566a
Brad Farr <v-bfarr@expedia.com>
2017-06-08 16:05:03 -0700

    testing codepipeline

--- src/main/java/com/codefellows/Application.java
+++ src/main/java/com/codefellows/Application.java
@@ -9,6 +9,8 @@ public class Application {
     }
 
     public static void main(String[] args) {
+        port(5000);
+
         get("/hello/:name", (request, response) -> {
             return "Hello: " + request.params(":name");
         });
