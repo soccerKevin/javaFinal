875d344844c901e511e645e3d1b6335769e5f939
soccerkevin <soccerkevint11@gmail.com>
2017-06-24 01:16:59 -0700

    scaling works, not centered

--- src/main/java/com/zoo/Animal.java
+++ src/main/java/com/zoo/Animal.java
@@ -9,6 +9,7 @@ public class Animal extends JPanel{
     private String name, imagePath;
     private Image image;
     private Dimension originalSize = new Dimension(100, 100);
+    private Point currentLocation;
     private UUID uuid;
     private double scale = 1;
 
@@ -19,8 +20,14 @@ public class Animal extends JPanel{
         this.image = new ImageIcon( imagePath ).getImage();
         this.uuid = UUID.randomUUID();
         setBackground(new Color(0, 0, 0, 0));
-        addMouseMotionListener(new AnimalMouseAdapter());
+        addMouseMotionListener(new MouseDragAdapter());
+        addMouseListener(new MouseActionAdapter());
         setVisible(true);
+    }
+
+    public void setLocation(Point location){
+        currentLocation = location;
+        super.setLocation(location);
     }
 
     public void setScale(double scale){
@@ -28,6 +35,10 @@ public class Animal extends JPanel{
         int w = (int) Math.round(scale * originalSize.width );
         int h = (int) Math.round(scale * originalSize.height );
         setSize(new Dimension(w, h));
+
+        int x = (int) Math.round(scale * currentLocation.x);
+        int y = (int) Math.round(scale * currentLocation.y);
+        super.setLocation(new Point(x, y));
     }
 
     public Animal softClone(){
@@ -44,7 +55,19 @@ public class Animal extends JPanel{
         g.drawImage(image, 0, 0, width(), height(), this);
     }
 
-    public class AnimalMouseAdapter extends MouseAdapter{
+    public class MouseActionAdapter extends MouseAdapter {
+        @Override
+        public void mousePressed(MouseEvent e) {
+            currentLocation = getLocation();
+        }
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            currentLocation = getLocation();
+        }
+
+    }
+
+    public class MouseDragAdapter extends MouseAdapter{
         @Override
         public void mouseDragged(MouseEvent e) {
             setLocation(e.getLocationOnScreen().x - 200, e.getLocationOnScreen().y - 100);
